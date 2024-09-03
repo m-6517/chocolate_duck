1aeab3c8c4976c49c7e46b8e77050f2ab3c27d0b
mana <m.komori6517@gmail.com>
2024-09-03 20:37:39 +0900

    fix: rubocop

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -21,7 +21,7 @@ class BoardImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   def default_url
-    'board_placeholder.png'
+    "board_placeholder.png"
   end
 
   # Process files as they are uploaded:
