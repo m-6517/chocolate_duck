bc6e52be97f58803eff367f0bf8f06101f7780b3
mana <m.komori6517@gmail.com>
2024-09-07 21:41:37 +0900

    fix: rubocop

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -12,7 +12,7 @@ class BoardImageUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
-  
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
