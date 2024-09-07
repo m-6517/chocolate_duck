1dc5da4e1d9a119c30e13c535c9403e46159398b
mana <m.komori6517@gmail.com>
2024-09-07 15:04:10 +0900

    add: reset_password ルーティングとコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root "static_pages#top"
   resources :users, only: %i[new create]
   resources :boards, only: %i[index new create show edit update destroy]
   resource :profile, only: %i[show edit update]
+  resources :password_resets, only: %i[new create edit update]
 
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
