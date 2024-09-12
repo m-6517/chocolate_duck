3ae1a1c6df21d266f6b53d6b61f4fff4aba831e5
mana <m.komori6517@gmail.com>
2024-09-12 10:21:15 +0900

    add: ページタイトル

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+
+  def page_title(title = '')
+    base_title = 'CHOCOLATE DUCK'
+    title.present? ? "#{title} | #{base_title}" : base_title
+  end
+  
   def flash_background_color(type)
     case type.to_sym
     when :success then "bg-yellow-100 border text-yellow-700 rounded-box relative"
