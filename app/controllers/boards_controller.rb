c1a71eed22eb5f98f44bcf36c4dd76b0827c6384
mana <m.komori6517@gmail.com>
2024-09-13 23:18:14 +0900

    add: metaタグの設定

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,7 @@
 class BoardsController < ApplicationController
+  ## privateに追加したprepare_meta_tagsをboardコントローラー以外にも使えるように設定
+  helper_method :prepare_meta_tags
+
   def index
     @boards = Board.includes(:user).order(created_at: :desc)
   end
@@ -19,6 +22,8 @@ class BoardsController < ApplicationController
 
   def show
     @board = Board.find(params[:id])
+    ## メタタグを設定
+    prepare_meta_tags(@board)
   end
 
   def edit
@@ -45,5 +50,24 @@ class BoardsController < ApplicationController
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
+  end
+
+  def prepare_meta_tags(post)
+    ## image_urlにMiniMagickで設定したOGPの生成した合成画像を代入する
+    image_url = "#{request.base_url}/images/ogp.png?text=#{CGI.escape(@board.title)}"
+    set_meta_tags og: {
+                    site_name: "CHOCOLATE DUCK",
+                    title: @board.title,
+                    description: "ちゃいろときいろ ずかん",
+                    type: "website",
+                    url: request.original_url,
+                    image: image_url,
+                    locale: "ja-JP"
+                  },
+                  twitter: {
+                    card: "summary_large_image",
+                    site: "@https://x.com/kumateq",
+                    image: image_url
+                  }
   end
 end
