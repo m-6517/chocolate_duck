3e23a5999bd1c79b5759d4e2ac39dbcf556262bf
mana <m.komori6517@gmail.com>
2024-09-15 08:51:36 +0900

    upload: OGPを動的→静的に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,29 +10,4 @@ module ApplicationHelper
     when :danger then "bg-orange-100 border text-orange-700 rounded-box relative"
     end
   end
-
-  def default_meta_tags
-    {
-      site: "CHOCOLATE DUCK",
-      title: "CHOCOLATE DUCK",
-      reverse: true,
-      charset: "utf-8",
-      description: "CHOCOLATE DUCK",
-      canonical: request.original_url,
-      og: {
-        site_name: "CHOCOLATE DUCK",
-        title: "CHOCOLATE DUCK",
-        description: "CHOCOLATE DUCK",
-        type: "website",
-        url: request.original_url,
-        image: image_url("ogp.png"),
-        local: "ja-JP"
-      },
-      twitter: {
-        card: "summary_large_image",
-        site: "@https://x.com/kumateq",
-        image: image_url("ogp.png")
-      }
-    }
-  end
 end
